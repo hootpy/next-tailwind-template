10e044cbedd4e495c5c5d5ba3690db71176137e2
Hootpy <dev@hootpy.com>
2023-05-31 23:46:49 +0700

    Initial commit from Create Next App

---
+++ README.md
@@ -0,0 +1,19 @@
+## Next-tailwind-template
+
+This project is create by NextJS, comes with TailwindCSS.
+
+When clone this project, you must install husky for pre-commit hooks.
+
+Install dependencies:
+
+```bash
+yarn install
+```
+
+Intall husky:
+
+```bash
+husky install
+```
+
+Then, you are free to go.
---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({ subsets: ['latin'] })
+
+export const metadata = {
+  title: "Trang chủ | Hootpy's Blog",
+  description:
+    'Normal gamer, a pythonista and an ordinary programmer! Welcome to my blog!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  )
+}
